ec30cfb27245877e9950c58024c127bb22b0a04c
sangsua <quang1994@web.de>
2021-02-04 05:41:36 +0100

    Change name of project and add js and css

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use regex::Regex;
 use std::env;
-
 fn main() -> Result<(), std::io::Error> {
     //get the first input for the name of the file
 
@@ -16,38 +15,55 @@ fn main() -> Result<(), std::io::Error> {
         "please insert a proper name without special characters and stuff"
     );
 
-    let html_file_name = format!("{}{}", input, ".html");
-
+    let directory_name = input;
     // simple protection dublication protection
     if let Ok(entries) = std::fs::read_dir(".") {
         for entry in entries {
             if let Ok(entry) = entry {
                 //everything is good, as long as the html_name doesn't equal the entries
                 //else the program terminates
-                assert!(entry.file_name() != html_file_name.as_str(),
+                assert!(entry.file_name() != directory_name.as_str(),
                 "the name of the file matches with another one.. please choose another name for your html");
             }
         }
     }
 
+    let html_file_name = format!("{}{}",directory_name,"/index.html");
+    let css_file = format!("{}{}",directory_name,"/style.css");
+    let js_script = format!("{}{}",directory_name,"/script.js");
     // finally create and write the content into the file
-    std::fs::write(html_file_name, content())?;
+    std::fs::create_dir(&directory_name)?;
+    std::fs::write(html_file_name, html_default_content())?;
+    std::fs::write(css_file, css_default_content())?;
+    std::fs::write(js_script, "")?;
     Ok(())
 }
 
-fn content() -> String {
-    //html template
+//html template
+pub fn html_default_content() -> String { 
     let content = "<!DOCTYPE html>
 <html lang=\"en\">
     <head>
        <meta charset=\"utf-8\">
        <title>title</title>
        <link rel=\"stylesheet\" href=\"style.css\">
+       <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+       <meta name=\"robots\" content=\"index,follow\"><!--helps so google can follow-->
        <script src=\"script.js\"></script>
     </head>
     <body>
-        <!-- page content -->
+        <header>
+        </header>        
+        <main>
+        </main>        
+        <footer>
+        </footer>    
     </body>
 </html>";
     content.to_string()
 }
+
+pub fn css_default_content()->String{
+    "*{ margin: 0; padding: 0; box-sizing: border-box; }".to_string()
+}
+
